2cb6a02969fea9f50e69d59bad311fa731d50f9c
Mathias <mathias.luecker@gmail.com>
2013-07-06 23:35:45 +0200

    read content of index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index_text = fs.readFileSync('index.html');
+
+var buffer = new Buffer(index_text, "utf-8")
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
